f887d86fedda8197140bebb1f1984e2454af0424
Rafael Reis Franco <87779561+faelreis@users.noreply.github.com>
2023-12-13 21:39:56 -0300

    Update app.module.ts

--- angular/curso-angular/src/app/app.module.ts
+++ angular/curso-angular/src/app/app.module.ts
@@ -1,37 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http'
+
+
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
-import { DadosPaiComponent } from './components/dados-pai/dados-pai.component';
-import { DiretivasComponent } from './components/diretivas/diretivas.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
+import { DirectivesComponent } from './components/directives/directives.component';
 import { IfRenderComponent } from './components/if-render/if-render.component';
 import { EventosComponent } from './components/eventos/eventos.component';
 import { EmitterComponent } from './components/emitter/emitter.component';
 import { ChangeNumberComponent } from './components/change-number/change-number.component';
 import { ListRenderComponent } from './components/list-render/list-render.component';
 import { PipesComponent } from './components/pipes/pipes.component';
-import { TwoWayBidingComponent } from './components/two-way-biding/two-way-biding.component';
+import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
+import { ItemDetailComponent } from './components/item-detail/item-detail.component';
+import { DataFormComponent } from './components/data-form/data-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponentComponent,
-    DadosPaiComponent,
-    DiretivasComponent,
+    ParentDataComponent,
+    DirectivesComponent,
     IfRenderComponent,
     EventosComponent,
     EmitterComponent,
     ChangeNumberComponent,
     ListRenderComponent,
     PipesComponent,
-    TwoWayBidingComponent,
+    TwoWayBindingComponent,
+    ItemDetailComponent,
+    DataFormComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
