a7f97f199e3c9538cb3aedaf705f4d7d76033f26
Rafael Reis Franco <87779561+faelreis@users.noreply.github.com>
2023-12-13 21:42:01 -0300

    Update app-routing.module.ts

--- angular/curso-angular/src/app/app-routing.module.ts
+++ angular/curso-angular/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { EmitterComponent } from './components/emitter/emitter.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
+import { DirectivesComponent } from './components/directives/directives.component';
+import { PipesComponent } from './components/pipes/pipes.component';
+import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
+import { ItemDetailComponent } from './components/item-detail/item-detail.component';
+import { DataFormComponent } from './components/data-form/data-form.component';
+
+
+const routes: Routes = [
+  {path: '', component: FirstComponentComponent},
+  {path: 'list', component: ListRenderComponent},
+  {path: 'list/:id', component: ItemDetailComponent},
+  {path: 'emitter', component:EmitterComponent},
+  {path: 'eventos', component:EventosComponent},
+  {path: 'if-render', component:IfRenderComponent},
+  {path: 'directives', component:DirectivesComponent},
+  {path: 'pipes', component:PipesComponent},
+  {path: 'two-way-binding', component:TwoWayBindingComponent},
+  {path: 'form', component: DataFormComponent}
+  
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
