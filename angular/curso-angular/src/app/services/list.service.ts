5c321c0dd2e513449d4f2cec1d258492261d9729
Rafael Reis Franco <rafaelreisfranco15@gmail.com>
2023-12-13 21:52:13 -0300

    Add exercices

---
+++ angular/curso-angular/src/app/services/list.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Animal } from '../Animal';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Contato } from '../Contato';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ListService {
+  private apiUrl = 'http://localhost:3000/animals';
+
+  constructor(private http: HttpClient) {}
+  remove(id: number) {
+    return this.http.delete<Animal>(`${this.apiUrl}/${id}`);
+  }
+
+  getAll(): Observable<Animal[]> {
+    return this.http.get<Animal[]>(this.apiUrl);
+  }
+
+  getItem(id: number): Observable<Animal> {
+    return this.http.get<Animal>(`${this.apiUrl}/${id}`);
+  }
+
+  setContato(contato: Contato): Observable<Contato> {
+    return this.http.post<Contato>(this.apiUrl, contato);
+  }
+}
